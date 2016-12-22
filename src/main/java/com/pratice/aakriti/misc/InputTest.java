11bc3942ca44f2c75cc1a76415dc833136ed4783
aakriti jain <aakriti.jain@gmail.com>
2016-12-22 15:37:28 +0530

    Saving my work

---
+++ src/main/java/com/pratice/aakriti/misc/InputTest.java
@@ -0,0 +1,24 @@
+package com.pratice.aakriti.misc;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+	
+	public static void main(String[] args) throws IOException {
+		int c;
+		try {
+			InputStream in = new LowerCaseIOStreamDeceorator(new BufferedInputStream(
+					new FileInputStream("src/main/java/test.txt")));
+			while((c = in.read()) >= 0 ) {
+				System.out.println((char)c);
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
