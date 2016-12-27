72cc5375b2e3b8aa259ec5bf83b94e4b49d4092a
aakriti jain <aakriti.jain@gmail.com>
2016-12-28 00:41:15 +0530

    Fixed a bug in insertion sort program and implemented method to sort in descending order as well.
    In insertion sort, we had a bug. Inner loop must test condition must be i > -1 not i > 0.

--- src/main/java/com/practice/aakriti/corman/InsertionSort.java
+++ src/main/java/com/practice/aakriti/corman/InsertionSort.java
@@ -7,7 +7,25 @@ public class InsertionSort {
 		for (int j = 1; j < myArray.length; j++) {
 			key = myArray[j];
 			int i = j - 1;
-			while (i > 0 && myArray[i] > key) {
+			while (i > -1 && myArray[i] > key) {
+				myArray[i + 1] = myArray[i];
+				i--;
+			}
+			myArray[i + 1] = key;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param myArray
+	 * @param sortAscending sort the array in ascending order if true, otherwise sort the array in descending order
+	 */
+	public void sortAscDesc(Integer[] myArray, boolean sortAscending) {
+		Integer key = null;
+		for (int j = 1; j < myArray.length; j++) {
+			key = myArray[j];
+			int i = j - 1;
+			while (i > -1 && (sortAscending ? myArray[i] > key : myArray[i] < key)) {
 				myArray[i + 1] = myArray[i];
 				i--;
 			}
--- src/test/java/com/pratice/aakriti/corman/InsertionSortTest.java
+++ src/test/java/com/pratice/aakriti/corman/InsertionSortTest.java
@@ -2,17 +2,28 @@ package com.pratice.aakriti.corman;
 
 import java.util.Arrays;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import com.practice.aakriti.corman.InsertionSort;
 
 public class InsertionSortTest {
-	   @Test
-	   public void test() {
-		   InsertionSort ins = new InsertionSort();
-		   Integer[] arrayTobeSorted = RandomNumberGenerator.generateArray();
-		   System.out.println("arrayTobeSorted is :" + Arrays.asList(arrayTobeSorted));
-		   ins.sort(arrayTobeSorted);
-		   System.out.println("sorted array is : "+ Arrays.asList(arrayTobeSorted));
-	   }
+	@Test
+	public void test() {
+		InsertionSort ins = new InsertionSort();
+		Integer[] arrayTobeSorted = RandomNumberGenerator.generateArray();
+		System.out.println("arrayTobeSorted is :" + Arrays.asList(arrayTobeSorted));
+		ins.sort(arrayTobeSorted);
+		System.out.println("sorted array is : " + Arrays.asList(arrayTobeSorted));
+	}
+
+	@Test
+	public void test1() {
+		InsertionSort ins = new InsertionSort();
+		Integer[] arrayTobeSorted = RandomNumberGenerator.generateArray();
+		System.out.println("arrayTobeSorted is :" + Arrays.asList(arrayTobeSorted));
+		ins.sortAscDesc(arrayTobeSorted, true);
+		System.out.println("sorted in asc order : " + Arrays.asList(arrayTobeSorted));
+		ins.sortAscDesc(arrayTobeSorted, false);
+		System.out.println("sorted in desc order : " + Arrays.asList(arrayTobeSorted));
+	}
 }
